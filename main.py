0757ec2a73f7e18e6fdb957c5d39b7a76755b3ea
ShawnSantosh <122426069+ShawnSantosh@users.noreply.github.com>
2024-03-28 08:38:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_authenticator as stauth 
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -12,53 +13,54 @@ from langchain.llms import HuggingFaceHub
 from PIL import Image
 import pickle
 from pathlib import Path
-import streamlit_authenticator as stauth 
 
- def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
+
+def get_pdf_text(pdf_docs):
+     text = ""
+     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
-    return text
+     return text
 
- def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
+
+def get_text_chunks(text):
+     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text)
-    return chunks
+     chunks = text_splitter.split_text(text)
+     return chunks
 
 
- def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+def get_vectorstore(text_chunks):
+      embeddings = OpenAIEmbeddings()
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+      vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+      return vectorstore
 
 
- def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
+def get_conversation_chain(vectorstore):
+     llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
-    memory = ConversationBufferMemory(
+     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
+     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
-    return conversation_chain
+     return conversation_chain
 
 
- def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+def handle_userinput(user_question):
+     response = st.session_state.conversation({'question': user_question})
+     st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
+     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
             st.write(user_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
@@ -67,28 +69,23 @@ import streamlit_authenticator as stauth
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
- def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+def main():
+     load_dotenv()
+     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
-    logo_image = st.image("ChatPDF LOGO.jpeg", width=100) 
-    
+     st.write(css, unsafe_allow_html=True)
 
-    if "conversation" not in st.session_state:
+     if "conversation" not in st.session_state:
         st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
+     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
+     st.header("Chat with multiple PDFs :books:")
+     user_question = st.text_input("Ask a question about your documents:")
+     if user_question:
         handle_userinput(user_question)
 
-    with st.sidebar:
-        authenticator.logout("Logout", "sidebar")
-        st.sidebar.title(f"Welcome{name}")
-        logo_image = st.image("PDF.png", width=50) 
+     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
             "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
@@ -106,7 +103,9 @@ import streamlit_authenticator as stauth
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(
                     vectorstore)
+                
 
+if __name__ == '__main__':
+     main()
 
- if __name__ == '__main__':
-    main()
+ 
